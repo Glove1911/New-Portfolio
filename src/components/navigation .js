a4c740deaffb6f850762145e27f7c2856ad0a3d8
Harrison Glover <harrison_glover@yahoo.com>
2021-02-18 09:30:29 -0600

    added scroll feature

--- src/components/navigation .js
+++ src/components/navigation .js
@@ -9,7 +9,7 @@ function Navigation(props) {
     return (
         <div className="navigation">
             <nav className="navbar navbar-expand-lg navbar-light">
-                <div className="container-fluid">
+                <div className="container-fluid overflow-auto">
                     <Link className="navbar-brand mr-5" to="#">Harrison L. Glover</Link>
                     <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
